34703eea6031431adb41dbeb5e3613e8b57cd12c
piratf <piratf.me@gmail.com>
2016-05-26 21:09:55 +0800

    优化了对分隔符丢失报错的处理，添加了出错后继续运行的功能

--- LexicalAnalyzer.hpp
+++ LexicalAnalyzer.hpp
@@ -120,41 +120,32 @@ class LexicalAnalyzer {
 
             memset(temp, 0, sizeof(char) * BUFFER_SIZE);
 
-            if (tag == "comment") {
-                strncpy(temp, head, tail -  head);
-                temp[tail - head] = 0;
-            } else {
-                int i = 0;
 
-                for (char *p = head; p != tail; ++p) {
-                    if (*p == '\n') {
-                        temp[i] = ' ';
-                    } else {
-                        temp[i] = *p;
-                    }
+            int i = 0;
 
-                    ++i;
+            for (char *p = head; p != tail; ++p) {
+                if (*p == '\n') {
+                    temp[i] = ' ';
+                } else {
+                    temp[i] = *p;
                 }
 
-                temp[i] = 0;
+                ++i;
             }
 
+            temp[i] = 0;
+
             // for signle line comment
-
-            // for multiline comment
-            if (temp[0] == '/' && temp[1] == '*') {
-
-                while (!(*tail == '*' && *(tail + 1) == '/') || !(*tail)) {
-                    if (*tail == '\n') {
-                        ++line_num;
-                    }
-
+            if (temp[0] == '/' && temp[1] == '/') {
+                while (*tail && *tail != '\n') {
                     ++tail;
                 }
 
-                strncpy(temp, head, tail -  head + 2);
+                ++line_num;
+
+                strncpy(temp, head, tail - head);
                 result_print("comment", temp);
-                head = tail + 2;
+                head = tail + 1;
 
                 while (
                     g_terminal_set.find(*head)
@@ -175,92 +166,22 @@ class LexicalAnalyzer {
                 tag.clear();
                 continue;
             }
-
-#ifdef DEBUG
-            printf("n = %d\n", tail - head);
-            printf("temp = %s\n", temp);
-            fflush(stdout);
-#endif
-
-            std::string cur = calculate(temp);
-
-            // matching failed
-            if (cur.empty()) {
-                failFlag = true;
-#ifdef DEBUG
-                printf("failed, temp = %s\n", temp);
-                fflush(stdout);
-#endif
-
-                // has a success before
-                if (!tag.empty()) {
-                    if (tag == "operator" ||
-                            ((!continuity(temp) ||
-                              _separator -> calculate(temp + strlen(temp) - 1))
-                             && strlen(output) == strlen(temp) - 1)) {
-
-                        failFlag = false;
-#ifdef DEBUG
-                        printf(" -----------> ");
-#endif
-                        // if (continuity(temp) && ) {
-
-                        // }
-                        result_print(tag.data(), output);
-                        fflush(stdout);
-
-                        head = tail - (strlen(temp) - strlen(output));
-
-                        while (
-                            g_terminal_set.find(*head)
-                            != g_terminal_set.end()) {
-
-                            if (*head == '\n') {
-                                ++line_num;
-                            }
-
-                            ++head;
-                        }
-
-#ifdef DEBUG
-                        printf("head = %c\n", *head);
-#endif
-                        tail = head;
-
-                        // correction
-                        // ...
-                        // end of correction
-
-                        vecTagList.push_back(tag);
-                        tag.clear();
-                    } else {
-                        continue;
-                    }
-                }
-
-            } else {
-                failFlag = false;
-#ifdef DEBUG
-                printf("success, tag = %s, temp = %s\n", cur.data(), temp);
-#endif
-                // 匹配成功
-                tag = cur;
-                strcpy(output, temp);
-#ifdef DEBUG
-                printf("output = %s\n", output);
-                fflush(stdout);
-#endif
-            }
-
         }
 
-        if (!tag.empty() && (temp == "operator" || strlen(temp) - 1 == strlen(output)) ) {
-#ifdef DEBUG
-            printf(" -----------> ");
-#endif
-            result_print(tag.data(), output);
+        // for multiline comment
+        if (temp[0] == '/' && temp[1] == '*') {
 
-            head = tail - (strlen(temp) - strlen(output));
+            while (!(*tail == '*' && *(tail + 1) == '/') || !(*tail)) {
+                if (*tail == '\n') {
+                    ++line_num;
+                }
+
+                ++tail;
+            }
+
+            strncpy(temp, head, tail -  head + 2);
+            result_print("comment", temp);
+            head = tail + 2;
 
             while (
                 g_terminal_set.find(*head)
@@ -272,54 +193,160 @@ class LexicalAnalyzer {
 
                 ++head;
             }
-        }
 
-        if (tail - head) {
-            err_shot("Unreachable sequences!");
-        } else {
-            printf("Analysis success!\n");
+#ifdef DEBUG
+            printf("head = %c\n", *head);
+#endif
+            tail = head;
+            // complete a matching
+            tag.clear();
+            continue;
         }
 
 #ifdef DEBUG
-        printf("head -> tail %d\n", tail - head);
+        printf("n = %d\n", tail - head);
+        printf("temp = %s\n", temp);
+        fflush(stdout);
 #endif
 
-        // }
+        std::string cur = calculate(temp);
 
-        ifile.close();
-    }
+        // matching failed
+        if (cur.empty()) {
+            failFlag = true;
+#ifdef DEBUG
+            printf("failed, temp = %s\n", temp);
+            fflush(stdout);
+#endif
 
-    void inline result_print(const char *tag, const char *content) {
-        printf("line %d: ( %10.10s : %-s )\n", line_num, tag, content);
-        fflush(stdout);
-    }
+            // has a success before
+            if (!tag.empty()) {
+                if (tag == "operator" ||
+                        ((!continuity(temp) ||
+                          _separator -> calculate(temp + strlen(temp) - 1))
+                         && strlen(output) == strlen(temp) - 1)) {
 
-    std::string calculate(const char *input) {
-        std::string tag;
+                    failFlag = false;
+#ifdef DEBUG
+                    printf(" -----------> ");
+#endif
+                    // if (continuity(temp) && ) {
 
-        for (auto *var : _vecRM) {
-            if (var -> calculate(input)) {
-                tag = var -> tag();
-                break;
+                    // }
+                    result_print(tag.data(), output);
+                    fflush(stdout);
+
+                    head = tail - (strlen(temp) - strlen(output));
+
+                    while (
+                        g_terminal_set.find(*head)
+                        != g_terminal_set.end()) {
+
+                        if (*head == '\n') {
+                            ++line_num;
+                        }
+
+                        ++head;
+                    }
+
+#ifdef DEBUG
+                    printf("head = %c\n", *head);
+#endif
+                    tail = head;
+
+                    // correction
+                    // ...
+                    // end of correction
+
+                    vecTagList.push_back(tag);
+                    tag.clear();
+                } else {
+                    continue;
+                }
             }
+
+        } else {
+            failFlag = false;
+#ifdef DEBUG
+            printf("success, tag = %s, temp = %s\n", cur.data(), temp);
+#endif
+            // 匹配成功
+            tag = cur;
+            strcpy(output, temp);
+#ifdef DEBUG
+            printf("output = %s\n", output);
+            fflush(stdout);
+#endif
         }
 
-        return tag;
     }
 
-    void err_shot(const char *message) {
-        printf("error** : %s line %d\n", message, line_num);
-        fflush(stdout);
+    if (!tag.empty() && (temp == "operator" || strlen(temp) - 1 == strlen(output)) ) {
+#ifdef DEBUG
+        printf(" -----------> ");
+#endif
+        result_print(tag.data(), output);
+
+        head = tail - (strlen(temp) - strlen(output));
+
+        while (
+            g_terminal_set.find(*head)
+            != g_terminal_set.end()) {
+
+            if (*head == '\n') {
+                ++line_num;
+            }
+
+            ++head;
+        }
     }
 
-    void separator(DFA *other) {
-        _separator = other;
+    if (tail - head) {
+        err_shot("Unreachable sequences!");
+    } else {
+        printf("Analysis success!\n");
     }
 
-  private:
-    std::vector<DFA *> _vecRM;
-    DFA *_separator;
-    unsigned int line_num;
+#ifdef DEBUG
+    printf("head -> tail %d\n", tail - head);
+#endif
+
+    // }
+
+    ifile.close();
+}
+
+void inline result_print(const char *tag, const char *content) {
+    printf("line %d: ( %10.10s : %-s )\n", line_num, tag, content);
+    fflush(stdout);
+}
+
+std::string calculate(const char *input) {
+    std::string tag;
+
+    for (auto *var : _vecRM) {
+        if (var -> calculate(input)) {
+            tag = var -> tag();
+            break;
+        }
+    }
+
+    return tag;
+}
+
+void err_shot(const char *message) {
+    printf("error** : %s line %d\n", message, line_num);
+    fflush(stdout);
+}
+
+void separator(DFA *other) {
+    _separator = other;
+}
+
+private:
+std::vector<DFA *> _vecRM;
+DFA *_separator;
+unsigned int line_num;
 };
 
 #endif
